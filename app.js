575e91b2e7d17ef3896202d9265c1a5ce4c34a31
荀潇 <xiao.xun@moji.com>
2020-05-26 15:19:59 +0800

    add cros

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+//设置允许跨域访问该服务.
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	//Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', '*');
+	res.header('Content-Type', 'application/json;charset=utf-8');
+	next();
+});
 app.use(timeout('8s'));
 app.use(logger('dev'));
 app.use(express.json());
